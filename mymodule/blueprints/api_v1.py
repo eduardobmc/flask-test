9413499c47d098fad1a4c380180094c9b740a13a
Eduardo Costa <eduardobmc@gmail.com>
2016-11-15 19:14:43 -0200

    Add upload route and associated tests.

--- mymodule/blueprints/api_v1.py
+++ mymodule/blueprints/api_v1.py
@@ -1,9 +1,20 @@
+import hashlib
 import flask
 from flask import current_app as app
 from werkzeug import exceptions
 
 
 blueprint = flask.Blueprint('v1', __name__, url_prefix='/api/v1')
+
+
+class OutputFile:
+    def __init__(self):
+        self.size = 0
+        self.checksum = hashlib.sha256()
+
+    def write(self, buf):
+        self.size += len(buf)
+        self.checksum.update(buf)
 
 
 @blueprint.route('/test')
@@ -23,15 +34,36 @@ def keys():
     return flask.jsonify(key=code)
 
 
-@blueprint.route('/crud')
-def crud_get():
-    return flask.jsonify(method=flask.request.method)
-
-
 @blueprint.route('/config')
 def config():
     ns = app.config.get_namespace('MYMODULE_')
     return flask.jsonify(ns)
+
+
+@blueprint.route('/upload', methods=['POST'])
+def upload():
+    results = []
+    storages = flask.request.files.getlist('files')
+    for storage in storages:
+        output = OutputFile()
+        storage.save(output)
+        result = {
+          'size': output.size,
+          'filename': storage.filename,
+          'sha256': output.checksum.hexdigest()
+        }
+        results.append(result)
+        app.logger.info(
+          'filename="%(filename)s" '
+          'size=%(size)d '
+          'sha256="%(sha256)s"' % result
+        )
+    return flask.jsonify(files=results)
+
+
+@blueprint.route('/crud')
+def crud_get():
+    return flask.jsonify(method=flask.request.method)
 
 
 @blueprint.route('/crud', methods=['POST'])
---
+++ tests/api_v1/test_upload.py
@@ -0,0 +1,46 @@
+import os
+import hashlib
+import pytest
+import test_base
+
+
+class FakeFile:
+    def __init__(self, size):
+        self.size = size
+        self.remaining = size
+        self.checksum = hashlib.sha256()
+
+    def read(self, n):
+        size = min(self.remaining, n)
+        self.remaining -= size
+        buf = os.urandom(size)
+        self.checksum.update(buf)
+        return buf
+
+
+class UploadTest(test_base.TestBase):
+    def test_post(self):
+        files = [(FakeFile(1 << 20), 'test.file')]
+        self._test(files)
+
+    @pytest.mark.slow
+    def test_large_post(self):
+        files = [(FakeFile(1 << 31), 'test.file')]
+        self._test(files)
+
+    def test_multiple_post(self):
+        files = [(FakeFile(1 << 20), 'test.%d' % i) for i in range(10)]
+        self._test(files)
+
+    def _test(self, files):
+        result = self.app.post('/api/v1/upload', data={'files': files})
+
+        self.assertOk(result)
+        self.assertJsonContentType(result)
+        self.assertJsonEquals(result, {
+          'files': [{
+            'size': f.size,
+            'filename': name,
+            'sha256': f.checksum.hexdigest()
+          } for f, name in files]
+        })
