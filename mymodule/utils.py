d2ace8f79aa589c418febd487fc5a361b2b7ef43
Eduardo Costa <eduardobmc@gmail.com>
2016-12-03 10:25:16 -0200

    New multipart parser and tests.

---
+++ mymodule/utils.py
@@ -0,0 +1,76 @@
+import re
+
+
+CRLF = b'\r\n'
+
+
+def read_until(reader, delim):
+    chunks = yield_until(reader, delim)
+    return b''.join(chunks)
+
+
+def read_headers(reader):
+    found = read_until_part(reader)
+    lines = filter(bool, found.split(CRLF))
+    raw_headers = {
+      h[0].lower(): h[1]
+      for h in (line.decode('ascii').split(': ') for line in lines)
+    }
+    content_disposition = raw_headers['content-disposition']
+    headers = {
+      k: v for k, v in directives(content_disposition)
+    }
+    headers['content-type'] = raw_headers['content-type']
+    return headers
+
+
+def directives(value):
+    r = re.compile(r';\s*([^=]+)="([^"]+)"')
+    for m in r.finditer(value):
+        yield m.group(1), m.group(2)
+
+
+def read_until_part(reader):
+    delim = CRLF * 2
+    data = read_until(reader, delim)
+    # reader.read(len(delim))
+    return data
+
+
+def parse_multipart(reader, boundary):
+    read_until(reader, boundary)
+    reader.read(len(boundary))
+
+    while True:
+        headers = read_headers(reader)
+        reader.read(2 * len(CRLF))
+
+        part = yield_until(reader, CRLF + boundary)
+        yield headers, part
+
+        reader.read(len(CRLF + boundary))
+        end = reader.peek()
+        if len(end) == 0 or end == b'--' + CRLF:
+            break
+
+
+def yield_until(reader, delim, size=65536):
+    chunk = _peek(reader, 2 * size)
+    while chunk:
+        index = chunk.find(delim)
+        if index == -1:
+            end = min(len(chunk), size)
+            yield chunk[:end]
+            reader.read(end)
+        else:
+            yield chunk[:min(index, size)]
+            if size < index:
+                yield chunk[size:index]
+            reader.read(index)
+            break
+        chunk = _peek(reader, 2 * size)
+
+
+def _peek(reader, size):
+    buf = reader.peek(size)
+    return buf[:min(size, len(buf))]
---
+++ tests/test_parser.py
@@ -0,0 +1,116 @@
+import io
+import pytest
+import unittest
+
+from mymodule import utils
+
+
+class YieldUntilTest(unittest.TestCase):
+    def setUp(self):
+        content = b'012|456|789|ab'
+        stream = io.BytesIO(content)
+        self.reader = io.BufferedReader(stream)
+
+    @pytest.mark.parse
+    def test_yield_until(self):
+        iterator = utils.yield_until(self.reader, b'6|7', size=4)
+        self.assertEquals(next(iterator), b'012|')
+        self.assertEquals(next(iterator), b'45')
+        self.assertRaises(StopIteration, next, iterator)
+        self.assertEquals(self.reader.read(), b'6|789|ab')
+
+    @pytest.mark.parse
+    def test_yield_size(self):
+        iterator = utils.yield_until(self.reader, b'9|a', size=4)
+        self.assertEquals(next(iterator), b'012|')
+        self.assertEquals(next(iterator), b'456|')
+        self.assertEquals(next(iterator), b'78')
+        self.assertRaises(StopIteration, next, iterator)
+        self.assertEquals(self.reader.read(), b'9|ab')
+
+    @pytest.mark.parse
+    def test_yield_upper_half(self):
+        iterator = utils.yield_until(self.reader, b'56', size=4)
+        self.assertEquals(next(iterator), b'012|')
+        self.assertEquals(next(iterator), b'4')
+        self.assertRaises(StopIteration, next, iterator)
+        self.assertEquals(self.reader.read(), b'56|789|ab')
+
+    @pytest.mark.parse
+    def test_yield_not_found(self):
+        iterator = utils.yield_until(self.reader, b'xyz', size=4)
+        self.assertEquals(next(iterator), b'012|')
+        self.assertEquals(next(iterator), b'456|')
+        self.assertEquals(next(iterator), b'789|')
+        self.assertEquals(next(iterator), b'ab')
+        self.assertRaises(StopIteration, next, iterator)
+
+
+class ReadUntilTest(unittest.TestCase):
+    def setUp(self):
+        content = b'abcdefgh'
+        stream = io.BytesIO(content)
+        self.reader = io.BufferedReader(stream)
+
+    def test_read_until(self):
+        self.assertEquals(utils.read_until(self.reader, b'efg'), b'abcd')
+        self.assertEquals(self.reader.read(), b'efgh')
+
+    def test_read_until_not_found(self):
+        self.assertEquals(utils.read_until(self.reader, b'xyz'), b'abcdefgh')
+        self.assertEquals(self.reader.read(), b'')
+
+
+class ParserTest(unittest.TestCase):
+    def test_headers(self):
+        data = (
+          b'Content-Disposition: form-data; name="files"; filename="test"\r\n'
+          b'Content-Type: application/octet-stream\r\n'
+          b'\r\n'
+          b'some data ...'
+        )
+        stream = io.BytesIO(data)
+        reader = io.BufferedReader(stream)
+        headers = utils.read_headers(reader)
+        self.assertEquals(headers, {
+          'name': 'files',
+          'filename': 'test',
+          'content-type': 'application/octet-stream'
+        })
+
+    def test_parser(self):
+        data = (
+          b'--boundary\r\n'
+          b'Content-Disposition: form-data; name="files"; filename="a"\r\n'
+          b'Content-Type: text/plain\r\n'
+          b'\r\n'
+          b'DATA A\r\n'
+          b'--boundary\r\n'
+          b'Content-Disposition: form-data; name="files"; filename="b"\r\n'
+          b'Content-Type: text/plain\r\n'
+          b'\r\n'
+          b'DATA B\r\n'
+          b'--boundary--\r\n'
+        )
+
+        stream = io.BytesIO(data)
+        reader = io.BufferedReader(stream)
+        parser = utils.parse_multipart(reader, b'--boundary')
+
+        h1, p1 = next(parser)
+        self.assertEquals(h1, {
+          'name': 'files',
+          'filename': 'a',
+          'content-type': 'text/plain'
+        })
+        self.assertEquals(b''.join(p1), b'DATA A')
+
+        h2, p2 = next(parser)
+        self.assertEquals(h2, {
+          'name': 'files',
+          'filename': 'b',
+          'content-type': 'text/plain'
+        })
+        self.assertEquals(b''.join(p2), b'DATA B')
+
+        self.assertRaises(StopIteration, next, parser)
